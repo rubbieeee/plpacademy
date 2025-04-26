1b4967fe13e8522365a07931fef9eca729bc58b2
rubbieeee <gatheruruth3@gmail.com>
2025-04-26 07:24:10 +0300

    lists.py

---
+++ lists.py
@@ -0,0 +1,22 @@
+#Question 1
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Question 2
+my_list[1] = 15
+
+#Question 3
+my_list.extend([50,60,70])
+
+#Question 4
+my_list.pop()
+
+#Question 5
+my_list.sort()
+
+#Question 6
+index_30 = my_list.index(30)
+print(index_30)
